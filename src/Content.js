07e4ec1496aad67a0a63befbc90a8c8d9ccd3c15
Iupac120 <100804858+Iupac120@users.noreply.github.com>
2022-10-17 15:36:13 +0100

    first commit

---
+++ src/Content.js
@@ -0,0 +1,61 @@
+import React,{useState} from 'react'
+import {FaTrashAlt} from 'react-icons/fa'
+
+const Content = () => {
+  
+  const [items,setItems] = useState([
+    {
+      id:1,
+      checked:false,
+      item:"One half bag of cocoa covered"
+    },
+    {
+      id:2,
+      checked:false,
+      item:"item two"
+    },
+    {
+      id:3,
+      checked:false,
+      item:"item three"
+    }
+  ])
+ 
+  const handleCheck =(id)=>{
+    const listItems = items.map((item)=>item.id === id? {...item,checked:!item.checked}:item)
+    setItems(listItems)
+    localStorage.setItem("shoppingList",JSON.stringify(listItems))
+  }
+
+  const handleDelete=(id)=>{
+    const listItems = items.filter((item)=>item.id !== id)
+    setItems(listItems)
+    localStorage.setItem("shoppingList",JSON.stringify(listItems))
+  }
+  
+  return (
+    <main>
+      <ul>
+        {items.map((item)=>(
+          <li className='item' key={item.id}>
+            <input
+                type="checkbox"
+                checked={item.checked}
+                onChange={()=>handleCheck(item.id)}
+            />
+            <label
+                style={(item.checked)?{textDecoration:"line-through"}:null}
+                onDoubleClick={()=>handleCheck(item.id)}
+            >{item.item}</label>
+            <FaTrashAlt
+                    onClick={()=>handleDelete(item.id)} 
+                    role="button" 
+                    tabIndex="0"/>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
+
+export default Content
